8443e0542050ad9d7a5d00d6f70942741d9c271f
Ayush <guptaayush630@gmail.com>
2020-07-27 15:47:55 +0530

    Creating CreateArea.jsx And Deleting notes.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes"
+import CreateArea from "./CreateArea";
 
-function createNotes(noteItem){
-    return(<Note 
-        key={noteItem.key}
-        title={noteItem.title}
-        content={noteItem.content}
-    />);
-}
-
-function App(){
-    return(
-        <div>
-            <Header />
-            {notes.map(createNotes)};
-            <Footer />
-        </div>
-    );
+function App() {
+  return (
+    <div>
+      <Header />
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
